3a1a9d07fed30c1c29229775266d4cb714c52b22
Gustavo Cortez <cmgustavo83@gmail.com>
2012-10-03 15:13:12 -0300

    tt support for multi mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -146,19 +146,21 @@
 ;; Muestra el numero de linea               
 (line-number-mode 1)
 
+(autoload 'tt-mode "tt-mode")
+(setq auto-mode-alist
+      (append '(("\\.tt$" . tt-mode))  auto-mode-alist ))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Multi web mode
 (require 'multi-web-mode)
 (setq mweb-default-major-mode 'html-mode)
 (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
 		  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-		  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+		  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")
+                  (tt-mode "\\[%" "%\\]")
+                  ))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5" "tt"))
 (multi-web-global-mode 1)
-
-(autoload 'tt-mode "tt-mode")
-(setq auto-mode-alist
-      (append '(("\\.tt$" . tt-mode))  auto-mode-alist ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Emacs Development Tools
@@ -172,38 +174,39 @@
 (add-to-list 'load-path "~/.emacs.d/pde")
 (load "pde-load")
 
-(ido-mode 1)
-(require 'template-simple)
+;; (ido-mode 1)
+;; (require 'template-simple)
 
 ;(load-file "~/.emacs.d/packages/ffap.el")
 ;(require 'ffap)
 ;(ffap-bindings)
 
 ;; M-SPC not available, window manager take it away
-(global-set-key (kbd "M-'") 'just-one-space)
-(global-set-key (kbd "C-M-=") 'pde-indent-dwim)
-;; nearest key to dabbrev-expand
-(global-set-key (kbd "M-;") 'hippie-expand)
-(global-set-key (kbd "C-;") 'comment-dwim)
-(global-set-key (kbd "C-c f") 'comint-dynamic-complete)
+;; (global-set-key (kbd "M-'") 'just-one-space)
+;; (global-set-key (kbd "C-M-=") 'pde-indent-dwim)
+;; ;; nearest key to dabbrev-expand
+;; (global-set-key (kbd "M-;") 'hippie-expand)
+;; (global-set-key (kbd "C-;") 'comment-dwim)
+;; (global-set-key (kbd "C-c f") 'comint-dynamic-complete)
 
-(setq hippie-expand-try-functions-list
-      '(try-expand-line
-        try-expand-dabbrev
-        try-expand-line-all-buffers
-        try-expand-list
-        try-expand-list-all-buffers
-        try-expand-dabbrev-visible
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-complete-file-name
-        try-complete-file-name-partially
-        try-complete-lisp-symbol
-        try-complete-lisp-symbol-partially
-        try-expand-whole-kill))
-(autoload 'comint-dynamic-complete "comint" "Complete for file name" t)
-(setq comint-completion-addsuffix '("/" . ""))
-(setq-default indent-tabs-mode nil)
+;; (setq hippie-expand-try-functions-list
+;;       '(try-expand-line
+;;         try-expand-dabbrev
+;;         try-expand-line-all-buffers
+;;         try-expand-list
+;;         try-expand-list-all-buffers
+;;         try-expand-dabbrev-visible
+;;         try-expand-dabbrev-all-buffers
+;;         try-expand-dabbrev-from-kill
+;;         try-complete-file-name
+;;         try-complete-file-name-partially
+;;         try-complete-lisp-symbol
+;;         try-complete-lisp-symbol-partially
+;;         try-expand-whole-kill))
+;; (autoload 'comint-dynamic-complete "comint" "Complete for file name" t)
+;; (setq comint-completion-addsuffix '("/" . ""))
+
+;; (setq-default indent-tabs-mode nil)
 
 ;; (defalias 'perl-mode 'cperl-mode)
 ;; (defun pde-perl-mode-hook ()
@@ -224,31 +227,31 @@
 ;;                  (cperl-tab-always-indent                     t)))
 ;;   (cperl-set-style "PDE"))
 
-(global-set-key (kbd "C-c s") 'compile-dwim-compile)
-(global-set-key (kbd "C-c r") 'compile-dwim-run)
-(setq compilation-buffer-name-function 'pde-compilation-buffer-name)
-(autoload 'compile-dwim-run "compile-dwim" "Build and run" t)
-(autoload 'compile-dwim-compile "compile-dwim" "Compile or check syntax" t)
-(autoload 'executable-chmod "executable"
-  "Make sure the file is executable.")
+;; (global-set-key (kbd "C-c s") 'compile-dwim-compile)
+;; (global-set-key (kbd "C-c r") 'compile-dwim-run)
+;; (setq compilation-buffer-name-function 'pde-compilation-buffer-name)
+;; (autoload 'compile-dwim-run "compile-dwim" "Build and run" t)
+;; (autoload 'compile-dwim-compile "compile-dwim" "Compile or check syntax" t)
+;; (autoload 'executable-chmod "executable"
+;;   "Make sure the file is executable.")
 
-(defun pde-perl-mode-hook ()
-  ;; chmod when saving
-  (when (and buffer-file-name
-             (not (string-match "\\.\\(pm\\|pod\\)$" (buffer-file-name))))
-    (add-hook 'after-save-hook 'executable-chmod nil t))
-  (set (make-local-variable 'compile-dwim-check-tools) nil))
+;; (defun pde-perl-mode-hook ()
+;;   ;; chmod when saving
+;;   (when (and buffer-file-name
+;;              (not (string-match "\\.\\(pm\\|pod\\)$" (buffer-file-name))))
+;;     (add-hook 'after-save-hook 'executable-chmod nil t))
+;;   (set (make-local-variable 'compile-dwim-check-tools) nil))
 
-(global-set-key (kbd "C-c i") 'imenu)
-(global-set-key (kbd "C-c v") 'imenu-tree)
-;;(global-set-key (kbd "C-c j") 'ffap)
+;; (global-set-key (kbd "C-c i") 'imenu)
+;; (global-set-key (kbd "C-c v") 'imenu-tree)
+;; ;;(global-set-key (kbd "C-c j") 'ffap)
 
 (setq tags-table-list '("~/TAGS"))
-(autoload 'imenu-tree "imenu-tree" "Show imenu tree" t)
-(setq imenu-tree-auto-update t)
-(eval-after-load "imenu"
-  '(defalias 'imenu--completion-buffer 'pde-ido-imenu-completion))
-(autoload 'tags-tree "tags-tree" "Show TAGS tree" t)
+;; (autoload 'imenu-tree "imenu-tree" "Show imenu tree" t)
+;; (setq imenu-tree-auto-update t)
+;; (eval-after-load "imenu"
+;;   '(defalias 'imenu--completion-buffer 'pde-ido-imenu-completion))
+;; (autoload 'tags-tree "tags-tree" "Show TAGS tree" t)
 ;; A wonderful minibuffer completion mode
 
 ;; (partial-completion-mode 1)
@@ -265,47 +268,47 @@
 ;;        ))
 ;; (add-to-list 'ffap-alist  '(cperl-mode . pde-ffap-locate))
 
-(defun perl-module-path (module-name)
-  (let* ((file-name
-          (concat (replace-regexp-in-string "::" "~/Documents/Github/Cinemaki/perl/CK/" module-name)
-                  ".pm"))
-         (command-line
-          (concat "perl -M'"
-                  module-name
-                  "' -e'print $INC{q{"
-                  file-name
-                  "}}'"))
-         (path (shell-command-to-string command-line))
-         (cant-locate (string-match "^Can't locate " path)))
-    (if cant-locate
-        nil
-      path)))
-(defun find-perl-module (module-name)
-  (interactive "sPerl module name: ")
-  (let ((path (perl-module-path module-name)))
-    (if path
-        (find-file path)
-      (error "Module '%s' not found" module-name))))
+;; (defun perl-module-path (module-name)
+;;   (let* ((file-name
+;;           (concat (replace-regexp-in-string "::" "~/Documents/Github/Cinemaki/perl/CK/" module-name)
+;;                   ".pm"))
+;;          (command-line
+;;           (concat "perl -M'"
+;;                   module-name
+;;                   "' -e'print $INC{q{"
+;;                   file-name
+;;                   "}}'"))
+;;          (path (shell-command-to-string command-line))
+;;          (cant-locate (string-match "^Can't locate " path)))
+;;     (if cant-locate
+;;         nil
+;;       path)))
+;; (defun find-perl-module (module-name)
+;;   (interactive "sPerl module name: ")
+;;   (let ((path (perl-module-path module-name)))
+;;     (if path
+;;         (find-file path)
+;;       (error "Module '%s' not found" module-name))))
 
 ;; Rebinding keys for hideshow
-(require 'hideshow)
-(define-key hs-minor-mode-map "\C-c\C-o"
-  (let ((map (lookup-key hs-minor-mode-map "\C-c@")))
-    ;; C-h is help to remind me key binding
-    (define-key map "\C-h" 'describe-prefix-bindings)
-    (define-key map "\C-q" 'hs-toggle-hiding)
-    ;; compatible with outline
-    (define-key map "\C-c" 'hs-hide-block)
-    (define-key map "\C-e" 'hs-show-block)
-    map))
+;; (require 'hideshow)
+;; (define-key hs-minor-mode-map "\C-c\C-o"
+;;   (let ((map (lookup-key hs-minor-mode-map "\C-c@")))
+;;     ;; C-h is help to remind me key binding
+;;     (define-key map "\C-h" 'describe-prefix-bindings)
+;;     (define-key map "\C-q" 'hs-toggle-hiding)
+;;     ;; compatible with outline
+;;     (define-key map "\C-c" 'hs-hide-block)
+;;     (define-key map "\C-e" 'hs-show-block)
+;;     map))
 
-(global-set-key (kbd "C-c h") 'help-dwim)
-(setq cperl-lazy-help-time 2)
-(defun pde-perl-mode-hook ()
-  (cperl-lazy-install))
+;; (global-set-key (kbd "C-c h") 'help-dwim)
+;; (setq cperl-lazy-help-time 2)
+;; (defun pde-perl-mode-hook ()
+;;   (cperl-lazy-install))
 
-(autoload 'run-perl "inf-perl" "Start perl interactive shell" t)
-(autoload 'perldb-ui "perldb-ui" "perl debugger" t)
+;; (autoload 'run-perl "inf-perl" "Start perl interactive shell" t)
+;; (autoload 'perldb-ui "perldb-ui" "perl debugger" t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Snippet (Autocomplete)
