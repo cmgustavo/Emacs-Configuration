54a40092cfbd7d9886911433b1717ddef911238a
Gustavo Cortez <cmgustavo83@gmail.com>
2012-11-13 14:30:57 -0300

    fixed emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -14,9 +14,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; TABS 
 
-;; Dont use mouse wheel
-(setq mouse-wheel-mode nil)
-
 ;; Include tabbar
 (require 'tabbar)
  (tabbar-mode)
@@ -93,26 +90,20 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Generalidades
 
-(setq mac-option-key-is-meta t)
+;; Dont use mouse wheel
+(setq mouse-wheel-mode nil)
 (setq mac-right-option-modifier nil)
 
 ;; Los archivos .bb son templates de SQL
 (setq auto-mode-alist (cons '("bb$" . sql-mode) auto-mode-alist))
 ;; La tecla 'Supr' borra la parte seleccionada
 (pending-delete-mode 1)
-;; Abre archivos comprimidos con gzip y bzip2
-(auto-compression-mode 1)
+
 ;; Algunas combinaciones de teclas
 (global-set-key "\C-g" 'goto-line)
 
 ;; Entra en modo de coloreado por sintaxis
 (global-font-lock-mode t)
-;; make it easy on eyes first ...
-;(set-foreground-color "gray")
-;(set-background-color "black")
-
-(setq font-lock-maximum-decoration t)
-(setq font-lock-maximum-size 262144)
 
 ;; Muestra pares de parentesis/corchetes/llaves       
 (require 'paren)
@@ -122,7 +113,7 @@
 ;; Mostrar los bloques marcados mientras los estamos marcando
 (setq transient-mark-mode t)
 ; Hacer scroll por 10 lineas              
-(setq scroll-step 20)
+(setq scroll-step 25)
 ;; No seguir agregando lineas en blanco al final
 (setq next-line-add-newlines nil)
 ;; Agregar automaticamente fin de linea a los archivos
@@ -136,11 +127,11 @@
 ;; Indenta por default en 4 tabs/espacios 
 (setq standard-indent 4)
 ;; Deshabilita los tab para indent
-(setq-default indent-tabs-mode nil)
+;(setq-default indent-tabs-mode nil)
 ;; Autocompletado de filas      
 (setq auto-fill-mode 1)
 ;; Muestra el numero de linea               
-(line-number-mode 1)
+;(line-number-mode 1)
 
 (autoload 'tt-mode "tt-mode")
 ;(setq auto-mode-alist
@@ -160,147 +151,21 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Emacs Development Tools
-;(load-file "~/.emacs.d/cedet/common/cedet.el")
-;(global-ede-mode 1)                      ; Enable the Project management system
-;(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
-;(global-srecode-minor-mode 1)            ; Enable template insertion menu
+(load-file "~/.emacs.d/cedet/common/cedet.el")
+(global-ede-mode 1)                      ; Enable the Project management system
+(semantic-load-enable-code-helpers)      ; Enable prototype help and smart completion 
+(global-srecode-minor-mode 1)            ; Enable template insertion menu
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; BEST PERL MODE
-(add-to-list 'load-path "~/.emacs.d/pde")
-(load "pde-load")
-
-;(ido-mode 1)
-;(require 'template-simple)
-
-;; M-SPC not available, window manager take it away
-;; (global-set-key (kbd "M-'") 'just-one-space)
-;; (global-set-key (kbd "C-M-=") 'pde-indent-dwim)
-;; ;; nearest key to dabbrev-expand
-;; (global-set-key (kbd "M-;") 'hippie-expand)
-;; (global-set-key (kbd "C-;") 'comment-dwim)
-;; (global-set-key (kbd "C-c f") 'comint-dynamic-complete)
-
-;; (setq hippie-expand-try-functions-list
-;;       '(try-expand-line
-;;         try-expand-dabbrev
-;;         try-expand-line-all-buffers
-;;         try-expand-list
-;;         try-expand-list-all-buffers
-;;         try-expand-dabbrev-visible
-;;         try-expand-dabbrev-all-buffers
-;;         try-expand-dabbrev-from-kill
-;;         try-complete-file-name
-;;         try-complete-file-name-partially
-;;         try-complete-lisp-symbol
-;;         try-complete-lisp-symbol-partially
-;;         try-expand-whole-kill))
-;; (autoload 'comint-dynamic-complete "comint" "Complete for file name" t)
-;; (setq comint-completion-addsuffix '("/" . ""))
-
-;; (setq-default indent-tabs-mode nil)
-
-;; (defalias 'perl-mode 'cperl-mode)
-;; (defun pde-perl-mode-hook ()
-;;   (abbrev-mode t)
-;;   (add-to-list 'cperl-style-alist
-;;                '("PDE"
-;;                  (cperl-auto-newline                          t)
-;;                  (cperl-brace-offset                          0)
-;;                  (cperl-close-paren-offset                    -4)
-;;                  (cperl-continued-brace-offset                0)
-;;                  (cperl-continued-statement-offset            4)
-;;                  (cperl-extra-newline-before-brace            nil)
-;;                  (cperl-extra-newline-before-brace-multiline  nil)
-;;                  (cperl-indent-level                          4)
-;;                  (cperl-indent-parens-as-block                t)
-;;                  (cperl-label-offset                          -4)
-;;                  (cperl-merge-trailing-else                   t)
-;;                  (cperl-tab-always-indent                     t)))
-;;   (cperl-set-style "PDE"))
-
-;; (global-set-key (kbd "C-c s") 'compile-dwim-compile)
-;; (global-set-key (kbd "C-c r") 'compile-dwim-run)
-;; (setq compilation-buffer-name-function 'pde-compilation-buffer-name)
-;; (autoload 'compile-dwim-run "compile-dwim" "Build and run" t)
-;; (autoload 'compile-dwim-compile "compile-dwim" "Compile or check syntax" t)
-;; (autoload 'executable-chmod "executable"
-;;   "Make sure the file is executable.")
-
-;; (defun pde-perl-mode-hook ()
-;;   ;; chmod when saving
-;;   (when (and buffer-file-name
-;;              (not (string-match "\\.\\(pm\\|pod\\)$" (buffer-file-name))))
-;;     (add-hook 'after-save-hook 'executable-chmod nil t))
-;;   (set (make-local-variable 'compile-dwim-check-tools) nil))
-
-;; (global-set-key (kbd "C-c i") 'imenu)
-;; (global-set-key (kbd "C-c v") 'imenu-tree)
-;; ;;(global-set-key (kbd "C-c j") 'ffap)
-
-;(setq tags-table-list '("~/TAGS"))
-;; (autoload 'imenu-tree "imenu-tree" "Show imenu tree" t)
-;; (setq imenu-tree-auto-update t)
-;; (eval-after-load "imenu"
-;;   '(defalias 'imenu--completion-buffer 'pde-ido-imenu-completion))
-;; (autoload 'tags-tree "tags-tree" "Show TAGS tree" t)
-;; A wonderful minibuffer completion mode
-
-;; (partial-completion-mode 1)
-;; (setq PC-include-file-path
-;;       (delete-dups (append PC-include-file-path pde-perl-inc)))
-
-;; (setq ffap-url-regexp
-;;       (concat
-;;        "\\`\\("
-;;        "news\\(post\\)?:\\|mailto:\\|file:" ; no host ok
-;;        "\\|"
-;;        "\\(ftp\\|https?\\|telnet\\|gopher\\|www\\|wais\\)://" ; needs host
-;;        "\\)[^:]" ; fix for perl module, require one more character that not ":"
-;;        ))
-;; (add-to-list 'ffap-alist  '(cperl-mode . pde-ffap-locate))
-
-;; (defun perl-module-path (module-name)
-;;   (let* ((file-name
-;;           (concat (replace-regexp-in-string "::" "~/Documents/Github/Cinemaki/perl/CK/" module-name)
-;;                   ".pm"))
-;;          (command-line
-;;           (concat "perl -M'"
-;;                   module-name
-;;                   "' -e'print $INC{q{"
-;;                   file-name
-;;                   "}}'"))
-;;          (path (shell-command-to-string command-line))
-;;          (cant-locate (string-match "^Can't locate " path)))
-;;     (if cant-locate
-;;         nil
-;;       path)))
-;; (defun find-perl-module (module-name)
-;;   (interactive "sPerl module name: ")
-;;   (let ((path (perl-module-path module-name)))
-;;     (if path
-;;         (find-file path)
-;;       (error "Module '%s' not found" module-name))))
-
-;; Rebinding keys for hideshow
-;; (require 'hideshow)
-;; (define-key hs-minor-mode-map "\C-c\C-o"
-;;   (let ((map (lookup-key hs-minor-mode-map "\C-c@")))
-;;     ;; C-h is help to remind me key binding
-;;     (define-key map "\C-h" 'describe-prefix-bindings)
-;;     (define-key map "\C-q" 'hs-toggle-hiding)
-;;     ;; compatible with outline
-;;     (define-key map "\C-c" 'hs-hide-block)
-;;     (define-key map "\C-e" 'hs-show-block)
-;;     map))
-
-;; (global-set-key (kbd "C-c h") 'help-dwim)
-;; (setq cperl-lazy-help-time 2)
-;; (defun pde-perl-mode-hook ()
-;;   (cperl-lazy-install))
-
-;; (autoload 'run-perl "inf-perl" "Start perl interactive shell" t)
-;; (autoload 'perldb-ui "perldb-ui" "perl debugger" t)
+;; Smart tabs (Autocomplete with TAB)
+(require 'smart-tab)
+; comment for use autocomplete by type of file
+(global-smart-tab-mode 1)
+(define-key read-expression-map [(tab)] 'hippie-expand)
+(defun hippie-unexpand ()
+  (interactive)
+  (hippie-expand 0))
+(define-key read-expression-map [(shift tab)] 'hippie-unexpand)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Snippet (Autocomplete)
@@ -310,14 +175,9 @@
 (yas-global-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Smart tabs (Autocomplete with TAB)
-(require 'smart-tab)
-(global-smart-tab-mode 1)
-(define-key read-expression-map [(tab)] 'hippie-expand)
-(defun hippie-unexpand ()
-  (interactive)
-  (hippie-expand 0))
-(define-key read-expression-map [(shift tab)] 'hippie-unexpand)
+;; BEST PERL MODE
+(add-to-list 'load-path "~/.emacs.d/pde")
+(load "pde-load")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Autoanadir parentesis o llaves de cierre
@@ -342,10 +202,10 @@
 ;; EMMS (Music Player)
 
 (add-to-list 'load-path "~/.emacs.d/emms/lisp/")
-;(add-to-list 'load-path (expand-file-name "~/.emacs.d/emms/lisp"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/emms/lisp"))
 
 (require 'emms-setup)
-(require 'emms-player-mplayer)
+;(require 'emms-player-mplayer)
 (emms-standard)
 (emms-default-players)
 
@@ -377,14 +237,13 @@
     (emms-add-directory-tree emms-source-file-default-directory)
      (message "Thud!")))
 
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (deeper-blue))))
+ '(custom-enabled-themes (quote (wheatgrass))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
